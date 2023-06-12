71548873545323c495394e93122472439aa0fd4c
MJ <code.za@proton.me>
2023-06-12 13:41:47 +0200

    toggle bug fix re ischecked starter

--- app/components/ThemeToggle.js
+++ app/components/ThemeToggle.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function ThemeToggle({ theme, toggleTheme }) {
-  const [isChecked, setIsChecked] = useState(false);
+  const [isChecked, setIsChecked] = useState(true);
 
   // handles both the toggle and the setting of theme
   const handleChange = () => {
@@ -12,7 +12,7 @@ export default function ThemeToggle({ theme, toggleTheme }) {
   return (
     <div className="flex flex-row justify-center items-center gap-3 pb-6 pl-4">
       <svg
-        className="dark:text-[#5A6069] text-white"
+        className="text-[#5A6069] dark:text-white"
         width="17"
         height="16"
         xmlns="http://www.w3.org/2000/svg"
@@ -33,7 +33,7 @@ export default function ThemeToggle({ theme, toggleTheme }) {
         <div className="w-11 h-6 bg-[#757575] rounded-full peer peer-focus:ring-gray-500  peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
       </label>
       <svg
-        className="text-[#5A6069] dark:text-white"
+        className="dark:text-[#5A6069] text-white"
         width="18"
         height="18"
         xmlns="http://www.w3.org/2000/svg"
